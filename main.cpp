7fc3215f2894c1c3588580ceb524c7d04dd3679f
matej <matobrecka@gmail.com>
2017-08-13 16:36:29 +0200

    Hello triangle

--- main.cpp
+++ main.cpp
@@ -2,14 +2,135 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#define DISPLAY_WIDTH 800
+#define DISPLAY_HEIGHT 600
+
+// ak niektore gl funkcie davaju segfault, mozno je ta funkcia z opengl ktoru som s GLEW neskontroloval ze mam
+
+const char *vertex_shader_source = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "\n"
+        "void main(){\n"
+        "    gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+        "}\0";
+
+const char *fragment_shader_source = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "\n"
+        "void main(){\n"
+        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+unsigned int VBO;
+unsigned int VAO;
+unsigned int vertex_shader;
+unsigned int fragment_shader;
+unsigned int shader_program;
+
+void init_shaders(){
+    int success;
+    char infoLog[512];
+
+    // vertex shader
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+
+    // vertex shader check
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // fragment shader
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+
+    // fragment shader check
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // shader program
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    // shader program check
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    glUseProgram(shader_program);
+
+    // cleanup
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+}
+
+void init() {
+
+    init_shaders();
+
+    // -----------------
+
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+    };
+
+    // trojuholnik
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+}
+
+void update() {
+
+}
+
+void render() {
+    // clear
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader_program);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
 int main() {
+    // glfw init
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(DISPLAY_WIDTH, DISPLAY_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -17,10 +138,41 @@ int main() {
     }
     glfwMakeContextCurrent(window);
 
+    // callbacky
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glew init
+    glewExperimental = GL_TRUE;
     GLenum err = glewInit();
     if (GLEW_OK != err) {
-        /* Problem: glewInit failed, something is seriously wrong. */
-        std::cout << "Error " << glewGetErrorString(err) << std::endl;
+        std::cout << "Error: " << glewGetErrorString(err) << std::endl;
+        return -1;
     }
+    std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << std::endl;
+    std::cout << "Status: OpenGL 3.3 supported=" << (GLEW_VERSION_3_3 != 0) << std::endl;
+
+    // open gl init
+    // TODO: zmensit viewport, nech sa mi tam vojde aj GUI
+    glViewport(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+    init();
+
+    // spustenie renderovania
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        process_input(window);
+
+        // update
+        update();
+
+        // render
+        render();
+
+        // end
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+
     return 0;
 }
